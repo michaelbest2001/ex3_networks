92618e5084ae3e8d3c711a89d9a5b70828e59756
unknown <micaelbest2001@gmail.com>
2025-01-11 12:41:44 +0200

    kkkk

--- cman_server.py
+++ cman_server.py
@@ -16,7 +16,7 @@ game = None
 
 server_socket = None  # Server socket for communication
 # List of clients and their roles
-clients = None
+clients = {}
 roles = {0:Player.NONE, 1:Player.CMAN, 2:Player.SPIRIT}  # Keep track of CMAN and SPIRIT
 
 spectators = []
@@ -110,6 +110,7 @@ def main():
     global game
     global server_socket
     global clients
+    
     # UDP server socket setup
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((SERVER_IP, SERVER_PORT))
